ee9574ac19efe4d9e42d7b608757f478d1bc8135
Кирилл <myakiishevkirill@gmail.com>
2023-08-25 16:20:46 +1000

    conducted API application refactoring

--- api/models.py
+++ api/models.py
@@ -17,5 +17,20 @@ class CourseResource(ModelResource):
         queryset = Course.objects.all()
         resource_name = 'courses'
         allowed_methods = ['get', 'post', 'delete']
+        excludes = ['created_at']
         authentication = CustomAuthentication()
         authorization = Authorization()
+        
+    def hydrate(self, bundle):
+        bundle.obj.category_id = bundle.data['category_id']
+        return bundle
+    
+    def dehydrate(self, bundle):
+        if bundle.request.method == 'GET':
+            bundle.data['reviews_qty'] = None
+            bundle.data['category_id'] = bundle.obj.category_id
+            bundle.data['category'] = bundle.obj.category
+        return bundle
+    
+    # def dehydrate_title(self, bundle):
+    #     return bundle.data['title'].upper()
